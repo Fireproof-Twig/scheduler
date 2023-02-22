658cdaa741aff68a0584281be1c148b0cb12e10b
Chris Baltis <cbaltis@gmail.com>
2023-02-22 15:20:33 -0500

    finished up to appointment component

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,28 @@
+import React from "react";
+import InterviewerListItem from "./InterviewerListItem";
+import "components/InterviewerList.scss";
+
+export default function InterviewerList(props) {
+  const interviewers = props.interviewers.map((interviewer) => {
+    return (
+      <InterviewerListItem
+        key={interviewer.id}
+        name={interviewer.name}
+        avatar={interviewer.avatar}
+        selected={interviewer.id === props.value}
+        setInterviewer={() => props.onChange(interviewer.id)}
+      />
+    );
+  });
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">Interviewer</h4>
+      <ul className="interviewers__list">{interviewers}</ul>
+    </section>
+  );
+}
+
+//////////////////////////props
+// interviewers:array - an array of objects as seen above
+// setInterviewer:function - a function that accepts an interviewer id. This function will simply be passed down to the <InterviewerListItem>
+// interviewer:number - a number that represents the id of the currently selected interviewer
